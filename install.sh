0c38a412b7beffed479f970df351bd813ecacf07
Amirali Dashti <devoft1@gmail.com>
2025-05-23 02:24:27 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ sudo chmod +x pypippark.sh
 sudo mkdir /usr/local/bin/pypippark
 
 sudo cp pypippark.sh /usr/local/bin/pypippark
+
+sudo ln -s /usr/local/bin/pypippark/pypippark.sh /usr/local/bin/pypippark
